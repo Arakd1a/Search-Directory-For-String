c317715cd95d0262a1eff3fcadbb634aae539728
Matt Barker <matt@barker.space>
2016-12-02 11:00:46 +1000

    Create XMLFindString.java

---
+++ XMLFindString.java
@@ -0,0 +1,24 @@
+package xml.find.string;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class XMLFindString {
+
+    ;
+
+    public static void main(String[] args) {
+        
+        Gui gui = new Gui();
+        gui.setVisible(true);
+
+      
+    }
+
+}
